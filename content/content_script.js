bd00f633d58895987b1ea39cb9793b842de3bcbf
robbie.hinch <robbie.hinch@e072b56a-e755-0410-b15a-53b2940fc0fb>
2010-03-25 07:55:18 +0000

    can display text in popup now

--- content/content_script.js
+++ content/content_script.js
@@ -1,8 +1,6 @@
 // Object to hold information about the current page
 var pageInfo = {
-    "title": document.title,
-    "url": window.location.href,
-    "summary": window.getSelection().toString()
+    "selected_text": window.getSelection().toString()
 };
 
 // Send the information back to the extension
